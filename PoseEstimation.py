acf9a216774d2c3d4b7246e1709f4229b687a382
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-04-28 20:37:29 +0530

    Instantiate pose detection model

--- PoseEstimation.py
+++ PoseEstimation.py
@@ -2,6 +2,10 @@ import cv2
 import time
 import mediapipe as mp
 
+
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+
 cap = cv2.VideoCapture('data/video_1.mp4')
 pTime = 0
 while True:
@@ -16,12 +20,14 @@ while True:
     resized_width = int(width/resize_ratio)
     resized_height = int(height/resize_ratio)
     img = cv2.resize(img, (resized_width, resized_height))
+    
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = pose.process(imgRGB)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
     cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
-    
     
     cv2.imshow('Image', img)
     cv2.waitKey(1)
